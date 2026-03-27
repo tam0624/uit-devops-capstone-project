8ff4395ce891804ba179c325c9ac162152e6e6a7
tam0624 <letamdinh46@gmail.com>
2026-03-27 09:50:40 +0700

    Cập nhật eks.tf chuẩn và hoàn thành mốc Tuần 3

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,31 +1,35 @@
-# 1. IAM Role cho EKS Cluster (Quyền để AWS quản lý Cluster)
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+  version = "~> 20.0"
 
   cluster_name    = "uit-devops-eks"
-  cluster_version = "1.27"
+  cluster_version = "1.29"
 
-  # Cho phép truy cập công cộng để nhóm dễ dàng quản lý bằng kubectl từ máy nhà
-  cluster_endpoint_public_access  = true
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
+  # PHẦN QUAN TRỌNG NHẤT: Tự động cấp quyền cho terraform-bot
+  enable_cluster_creator_admin_permissions = true
 
-  # Cấu hình Managed Node Groups (Các máy chủ chạy Microservices)
   eks_managed_node_groups = {
-    self_healing_nodes = {
-      min_size     = 2  # Luôn duy trì ít nhất 2 node để chịu lỗi
-      max_size     = 4
+    main = {
+      min_size     = 2
+      max_size     = 3
       desired_size = 2
 
-      instance_types = ["t3.medium"] # Cấu hình vừa đủ cho đồ án sinh viên
-      capacity_type  = "ON_DEMAND"
+      instance_types = ["t3.small"] # Tiết kiệm tiền cho sinh viên
+      capacity_type  = "SPOT"        # Rẻ hơn 70% và giúp báo cáo tính năng Self-healing
+
+      labels = {
+        Environment = "dev"
+        Project     = "online-boutique"
+      }
     }
   }
 
   tags = {
     Environment = "dev"
-    Project     = "self-healing-system"
+    Terraform   = "true"
   }
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0" # Sử dụng phiên bản AWS provider mới nhất
     }
   }
+  backend "s3" {
+    bucket         = "uit-devops-terraform-state-unique" # Tên bucket bạn vừa tạo
+    key            = "global/s3/terraform.tfstate"       # Đường dẫn file trong bucket
+    region         = "ap-southeast-1"
+    dynamodb_table = "terraform-state-locking"           # Tên table DynamoDB vừa tạo
+    encrypt        = true                                # Mã hóa file để bảo mật
+  }
+
 }
 
 provider "aws" {
